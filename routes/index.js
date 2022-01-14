7573c6c4d0bb3825c2987004145137a8c8bd4e54
DAMMIKA940 <dammikapadmasiri123@gmail.com>
2022-01-14 14:12:45 +0530

    add like controller,route

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,18 @@ const routes = require('express').Router();
 
 const userRoute = require('./mainRoute/userRoutes');
 const commentRoute = require('./mainRoute/commentRoute');
+const likeRoute = require('./mainRoute/likeRoute');
 /*const videoRoute = require('./mainRoute/videoRoutes');
-const likeRoute = require('./mainRoute/likeRoutes');
+
 
 const categoryRoute = require('./mainRoute/categoryRoute');
 const subcategoryRoute = require('./mainRoute/subcategoryRoute');*/
 
 routes.use('/user', userRoute);
 routes.use('/comment', commentRoute);
-/*routes.use('/video', videoRoute);
 routes.use('/like', likeRoute);
+/*routes.use('/video', videoRoute);
+
 routes.use('/comment', commentRoute);
 routes.use('/category', categoryRoute);
 routes.use('/subcategory', subcategoryRoute);*/
